d305930aafd01ffaf7b91503810069c4de01134d
Abhishek Sharma <asharma991@outlook.com>
2023-08-05 01:30:56 +0530

    Test husky

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Logo from './react-icon.svg';
 import ClickCounter from './ClickCounter';
 
 export const App = () => {
+  const name = 'abhishek';
   return (
     <>
       <h1>
-        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{' '}
-        {process.env.name}
+        React TypeScript Webpack Starter Template - {name} {process.env.name}
       </h1>
       ;
       <img src={Img} alt='Web API' width='300' height='200' />
