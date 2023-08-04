df6ec3de19ec0611ab019b92b6427ef98bb85c69
Abhishek Sharma <asharma991@outlook.com>
2023-08-04 10:57:18 +0530

    minor style and env usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import "./styles.css";
+import Img from "./web-api.png";
+import Logo from "./react-icon.svg";
+
 export const App = () => {
-  return <h1>React TypeScript Webpack Starter Template</h1>;
+  return (
+    <>
+      <h1>
+        React TypeScript Webpack Starter Template - {process.env.NODE_ENV}{" "}
+        {process.env.name}
+      </h1>
+      ;
+      <img src={Img} alt='Web API' width='300' height='200' />
+      <img src={Logo} alt='React Logo' width='300' />
+    </>
+  );
 };
